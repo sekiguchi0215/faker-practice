8d4839ef0525103b808b98545746454d0205f99d
vagrant <vagrant@localhost.localdomain>
2020-07-01 13:35:35 +0000

    faker practice

---
+++ db/migrate/20200701130356_add_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :password, :string
+    add_column :users, :password_confirmation, :string
+  end
+end
